4e5008102747e0570c64ac291abfe4d5c9eb6a09
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-06-08 17:08:12 +0500

    Composing Components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,24 +1,43 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0, tags: ["tag1", "tag2", "tag3"] };
+  state = { count: 0, tags: ["tag1", "tag2", "tag3", "tag4"] };
   styles = {
     fontSize: 50,
     fontWeight: "bold",
   };
+
+  rednerTags() {
+    if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked!", this.state.count);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span style={this.styles} className={this.getBadgeClass()}>
           {this.formatCounter()}
         </span>
-        <button className={"btn btn-secondary btn-lg"}>Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+        <button
+          onClick={() => this.handleIncrement()}
+          className={"btn btn-secondary btn-lg"}
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create the new tag!"}
+        {this.rednerTags()}
+      </div>
     );
   }
   getBadgeClass() {
