a9b495b7bc0909611549a53fa45cd9cd0a1ab04a
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-06-29 16:45:23 +0500

    Navbar Added and everythings work fine

---
+++ counter-app/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <nav className="navbar bg-info">
+          <h1>1</h1>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Counters />
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
