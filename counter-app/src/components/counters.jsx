551b814bb9a0356fc3bf2786dc3d576669358d1f
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-06-29 13:09:51 +0500

    Update counters.jsx

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -5,9 +5,9 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
+      { id: 2, value: 3 },
+      { id: 3, value: 1 },
+      { id: 4, value: 5 },
     ],
   };
 
